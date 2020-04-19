e1b4de1d338962c157d41c9751ebe6a412abd15e
B <philoman@gmail.com>
2020-04-19 13:36:36 +0800

    fix - dispose animation controller when disposing _TinderSwapCardState

--- lib/flutter_tindercard.dart
+++ lib/flutter_tindercard.dart
@@ -247,6 +247,12 @@ class _TinderSwapCardState extends State<TinderSwapCard>
     return Stack(children: _buildCards(context));
   }
 
+  @override
+  dispose() {
+    this._animationController.dispose();
+    super.dispose();
+  }
+
   changeCardOrder() {
     setState(() {
       _currentFront--;
